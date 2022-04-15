76b0e3a607281dacfa9d8eabcafb35b952fcad7a
Daniel Gerbig <degrbg@gmail.com>
2022-04-15 14:47:39 -0700

    update

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <memory>
 #include <set>
 
+int BResourceID = 0;
+int BAllocatorID = 0;
+
 template <typename T, std::size_t ts>
 void type_reg_T() {
   return;
@@ -16,7 +19,10 @@ void type_reg_TU() {
 }
 
 struct BResource {
-  BResource() {}
+  BResource() : id_{BResourceID} {
+    std::cout << "New BResource " << id_ << "\n";
+    ++BResourceID;
+  }
   BResource(const BResource& other) = delete;
   BResource& operator=(const BResource& other) = delete;
 
@@ -31,11 +37,14 @@ struct BResource {
     if (it != ptrs_.end()) {
       ptrs_.erase(it);
     } else {
-      std::cout << "BResource::deallocate(): p not found\n";
+      std::cout
+          << "BResource::deallocate(): p not found in BResource "
+          << id_ << "\n";
     }
     std::free(p);
   }
 
+  const int id_;
   std::set<void*> ptrs_;
 };
 
@@ -44,36 +53,50 @@ struct BAllocator {
   using value_type = T;
 
   BAllocator() :
+      id_{BAllocatorID},
       ts_{sizeof(T)},
       us_{},
       res_{std::make_shared<BResource>()} {
+    std::cout << "New BAllocator " << id_
+        << " with BResource " << res_->id_ << "\n";
+    ++BAllocatorID;
     type_reg_T<T, sizeof(T)>();
   }
 
   BAllocator(const BAllocator& other) :
+      id_{BAllocatorID},
       ts_{other.ts_},
       us_{other.us_},
       res_{other.res_} {
+    std::cout << "New BAllocator " << id_
+        << " with BResource " << res_->id_ << "\n";
+    ++BAllocatorID;
     type_reg_T<T, sizeof(T)>();
   }
 
   template <typename U>
   BAllocator(const BAllocator<U>& other) :
+      id_{BAllocatorID},
       ts_{sizeof(T)},
       us_{sizeof(U)},
       res_{std::make_shared<BResource>()} {
+    std::cout << "New BAllocator " << id_
+        << " with BResource " << res_->id_ << "\n";
+    ++BAllocatorID;
     type_reg_TU<T, sizeof(T), U, sizeof(U)>();
   }
 
   T* allocate(std::size_t n) {
-    std::cout << "BAllocator::allocate() "
-        << n << " chunk(s) of size " << ts_ << "\n";
     void* p = res_->allocate(ts_ * n);
-    return static_cast<T*>(p);
+    auto tp = static_cast<T*>(p);
+    std::cout << "BAllocator " << id_ << " allocate() "
+        << n << " chunk(s) of size " << ts_ << " at "
+        << tp << "\n";
+    return tp;
   }
 
   void deallocate(T* tp, std::size_t n) {
-    std::cout << "BAllocator::deallocate() "
+    std::cout << "BAllocator " << id_ << " deallocate() "
         << n << " chunk(s) of size " << ts_ << " at "
         << tp << "\n";
     void* p = static_cast<void*>(tp);
@@ -92,6 +115,7 @@ struct BAllocator {
     return !(*this == other);
   }
 
+  const int id_;
   const std::size_t ts_;
   const std::size_t us_;
   std::shared_ptr<BResource> res_;
